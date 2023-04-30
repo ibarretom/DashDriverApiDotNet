d29c75dfbc69c51ceb184289c7c04dc63c6c654b
ibarretom <igorbm.23@gmail.com>
2023-04-30 20:16:31 -0300

    Adição de banco de dados e classe Bootstrper para injetar o Contexto Entity Framework.

---
+++ src/Infra/Bootstraper.cs
@@ -0,0 +1,17 @@
+﻿using Infra.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra;
+
+public static class Bootstraper
+{
+    public static void AddInfra(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DashDriverContext>(options =>
+        {
+            options.UseMySql(configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 26)));
+        });
+    }
+}
---
+++ src/Infra/Database/DashDriverContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Database;
+
+internal class DashDriverContext : DbContext
+{
+    public DashDriverContext(DbContextOptions<DashDriverContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DashDriverContext).Assembly);
+    }
+}
