6af4c215898d2417c95a0274a8cb1237e876d1b6
ibarretom <igorbm.23@gmail.com>
2023-04-30 22:08:34 -0300

    Criação de entidade de usuário no modelo do Entity Framework

---
+++ src/Core/Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Domain.Entities;
+
+[Table("user")]
+[Index(nameof(Email), IsUnique = true)]
+public class User
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+
+
+    [Column("email")]
+    public string Email { get; set; }
+
+    [Column("password")]
+    public string Password { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
