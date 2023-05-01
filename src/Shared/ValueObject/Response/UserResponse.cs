53bbf828ba7f277162c07a4168d93057c443abef
ibarretom <igorbm.23@gmail.com>
2023-05-01 19:31:32 -0300

    Criação de Objeto que será retornado no serviço de SignUp

---
+++ src/Shared/ValueObject/Response/UserResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Shared.ValueObject.Response;
+
+public class UserResponse
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhotoUrl { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
