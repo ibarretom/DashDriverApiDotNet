1a79a452882f7d4333071ca3a354136e3cb8b3f9
ibarretom <igorbm.23@gmail.com>
2023-05-01 19:30:48 -0300

    Setup de variaveis para realizar teste

---
+++ src/Shared/test/ValueObject/UserDTOTest.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using Shared.ValueObject.DTO;
+
+namespace Shared.test.ValueObject;
+
+public static class UserDTOTest
+{
+    public static UserDTO Build()
+    {
+        return new Faker<UserDTO>()
+            .RuleFor(u => u.Name, f => f.Person.FullName)
+            .RuleFor(u => u.Email, f => f.Internet.Email())
+            .RuleFor(u => u.Password, f => f.Internet.Password());
+    }
+
+}
