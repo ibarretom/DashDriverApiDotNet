5942350a393f7d4aeb339805aab7ac75b04fa65f
ibarretom <igorbm.23@gmail.com>
2023-05-06 16:03:45 -0300

    Validação dos campos do UserDTO com FluentValidator

---
+++ src/Core/Domain/Services/Validation/UserDTOValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Shared;
+using Shared.ValueObject.DTO;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CoreTest")]
+namespace Core.Domain.Services.Validation;
+
+internal class UserDTOValidation : AbstractValidator<UserDTO>
+{
+    public UserDTOValidation()
+    {
+        RuleFor(obj => obj.Name).NotEmpty().WithMessage(ErrorMessagesResource.NAME_BLANK);
+        RuleFor(obj => obj.Email).NotEmpty().WithMessage(ErrorMessagesResource.EMAIL_BLANK);
+        RuleFor(obj => obj.Password).NotEmpty().WithMessage(ErrorMessagesResource.PASSWORD_BLANK);
+        When(obj => !string.IsNullOrEmpty(obj.Email), () =>
+        {
+            RuleFor(obj => obj.Email).EmailAddress().WithMessage(ErrorMessagesResource.EMAIL_INVALID);
+        });
+    }
+}
---
+++ test/CoreTest/Domain/Services/Validation/UserDTOValidationTest.cs
@@ -0,0 +1,92 @@
+﻿using Core.Domain.Services.Validation;
+using FluentValidation.Results;
+using Shared;
+using Shared.ValueObject.DTO;
+using TestUtils.ValueObject;
+
+namespace CoreTest.Domain.Services.Validation;
+
+public class UserDTOValidationTest
+{
+    [Fact]
+    public void ShouldNotHaveErrorWhenFieldsAreValid()
+    {
+        //Setup
+        UserDTO userDTO = UserDTOTest.Build();
+
+        //Execution
+        UserDTOValidation validation = new UserDTOValidation();
+        ValidationResult result = validation.Validate(userDTO);
+        
+        //Assertion
+        Assert.True(result.IsValid);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenNameIsBlank()
+    {
+        //Setup
+        UserDTO userDTO = UserDTOTest.Build();
+        userDTO.Name = string.Empty;
+        
+        //Execution
+        UserDTOValidation validation = new UserDTOValidation();
+        ValidationResult result = validation.Validate(userDTO);
+        
+        //Assertion
+        Assert.False(result.IsValid);
+        Assert.True(result.Errors.Count == 1);
+        Assert.Equal(ErrorMessagesResource.NAME_BLANK, result.Errors[0].ErrorMessage);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenEmailIsBlank()
+    {
+        //Setup
+        UserDTO userDTO = UserDTOTest.Build();
+        userDTO.Email = string.Empty;
+        
+        //Execution
+        UserDTOValidation validation = new UserDTOValidation();
+        ValidationResult result = validation.Validate(userDTO);
+        
+        //Assertion
+        Assert.False(result.IsValid);
+        Assert.True(result.Errors.Count == 1);
+        Assert.Equal(ErrorMessagesResource.EMAIL_BLANK, result.Errors[0].ErrorMessage);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenPasswordIsBlank()
+    {
+        //Setup
+        UserDTO userDTO = UserDTOTest.Build();
+        userDTO.Password = string.Empty;
+        
+        //Execution
+        UserDTOValidation validation = new UserDTOValidation();
+        ValidationResult result = validation.Validate(userDTO);
+        
+        //Assertion
+        Assert.False(result.IsValid);
+        Assert.True(result.Errors.Count == 1);
+        Assert.Equal(ErrorMessagesResource.PASSWORD_BLANK, result.Errors[0].ErrorMessage);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenEmailIsInvalid()
+    {
+        //Setup
+        UserDTO userDTO = UserDTOTest.Build();
+        userDTO.Email = "invalid_email";
+        
+        //Execution
+        UserDTOValidation validation = new UserDTOValidation();
+        ValidationResult result = validation.Validate(userDTO);
+        
+        //Assertion
+        Assert.False(result.IsValid);
+        Assert.True(result.Errors.Count == 1);
+        Assert.Equal(ErrorMessagesResource.EMAIL_INVALID, result.Errors[0].ErrorMessage);
+    }
+}
