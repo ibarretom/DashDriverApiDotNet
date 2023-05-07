e9c32882a7ae16f2a5ca30b1573bd71c74984b6b
ibarretom <igorbm.23@gmail.com>
2023-05-06 22:03:36 -0300

    Adição da classe Bootstrap para injetar dependências e o pacote AutoMapper...DependencyInjection

---
+++ src/Core/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using Core.Application.Services.Authentication;
+using Core.Application.UseCases.Authentication;
+using Core.Domain.Services.Mappers;
+using Microsoft.Extensions.DependencyInjection;
+namespace Core;
+
+public static class Bootstrap
+{
+    public static void AddCoreApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+        services.AddScoped<ISignUpUseCase, SignUpUseCase>();
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddScoped<IPasswordHashService, BcryptDotNet>();
+    }
+}
